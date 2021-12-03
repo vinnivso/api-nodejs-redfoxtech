0aa890aaf4d0894de2b07d6694256911e7d19da6
vinnivso <vinnivsob@gmail.com>
2021-12-03 16:03:29 -0300

    Feat: entities developed, considering the recommendations in VSCODE

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { app } from "./app"
+// import { createProducts } from "./endpoints/createProducts"
+// import { getProducts } from "./endpoints/getProducts"
+
+// app.get("/products", getProducts)
+// app.post("/products", createProducts)
